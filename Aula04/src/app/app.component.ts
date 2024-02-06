4bcc26a9c4b326f7a43a0d1cef5c2a7c80feb394
devGusta2 <gustavo.souza2979@gmail.com>
2024-02-06 17:23:47 -0300

    Compartilhamento de Dados

--- Aula04/src/app/app.component.ts
+++ Aula04/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DadosComponent } from './components/dados/dados.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  username="Gustavo";
+  userData={
+    email:"Gustavo@gmail.com",
+    role:"Admin",
+  }
+
   title = 'Aula04';
 }
--- Aula04/src/app/components/dados/dados.component.ts
+++ Aula04/src/app/components/dados/dados.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-dados',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './dados.component.css'
 })
 export class DadosComponent {
-
+  @Input() name:String = "";//iniciado vazio
+  @Input()userData!:{email:String, role:String}// nao está iniciado no momento mas vai ser inicializado
+  
 }
