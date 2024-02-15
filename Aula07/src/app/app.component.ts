11a6efc6183b27a20797162ec8a283a0b165449f
devGusta2 <gustavo.souza2979@gmail.com>
2024-02-15 20:54:28 -0300

    Renderizacao-Condicional

--- Aula07/src/app/app.component.ts
+++ Aula07/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IfRenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
