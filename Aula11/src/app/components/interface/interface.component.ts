3e4006ea6846c5d4e4ede9050104a047b4a2c459
devGusta2 <gustavo.souza2979@gmail.com>
2024-02-21 20:19:03 -0300

    Interface

--- Aula11/src/app/components/interface/interface.component.ts
+++ Aula11/src/app/components/interface/interface.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Animal } from '../../Animal';
 @Component({
   selector: 'app-interface',
   standalone: true,
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './interface.component.css'
 })
 export class InterfaceComponent {
-
+  animalDetails='';
+  showAge(Animal:Animal){
+    this.animalDetails="O pet ${animal.name} tem ${animal.age} anos!"
+  }
+  animals:Animal[]=[
+    {name:"Mel", type:"Dog", age:9},
+    {name:"Scooby", type:"Dog", age:5}
+  ]
 }
