2368c5167e08159f8ba3b16c27f326b96164a39d
devGusta2 <gustavo.souza2979@gmail.com>
2024-02-21 20:05:45 -0300

    Interface

--- Aula11/src/app/app.component.ts
+++ Aula11/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InterfaceComponent } from './components/interface/interface.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InterfaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
