7b0b3c323158a0abd4342cdc3975fb847c3b37cc
devGusta2 <gustavo.souza2979@gmail.com>
2024-02-27 19:51:38 -0300

    Piper Operator

--- Aula12/src/app/components/pipe/pipe.component.ts
+++ Aula12/src/app/components/pipe/pipe.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import {UpperCasePipe} from '@angular/common';
+import {UpperCasePipe, TitleCasePipe} from '@angular/common';
+
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, TitleCasePipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
 export class PipeComponent {
   texto:string="Bom dia";
+  mensagem:string="titulo em title case";
 }
