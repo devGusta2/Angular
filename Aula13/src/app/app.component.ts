212d5499dab08cf57812f3999376df54ee8f3883
devGusta2 <gustavo.souza2979@gmail.com>
2024-02-29 17:40:45 -0300

    Two-way-dataBind

--- Aula13/src/app/app.component.ts
+++ Aula13/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule}from '@angular/forms'
+import { TwoWayDataBindComponent } from './components/two-way-data-bind/two-way-data-bind.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TwoWayDataBindComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Aula13/src/app/components/two-way-data-bind/two-way-data-bind.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-data-bind',
+  standalone: true,
+  imports: [],
+  templateUrl: './two-way-data-bind.component.html',
+  styleUrl: './two-way-data-bind.component.css'
+})
+export class TwoWayDataBindComponent {
+  name:string="oi";
+
+}
