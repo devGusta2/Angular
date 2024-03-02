30d4890a309a5569968022bbec0d6d4f6d7f9810
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-02 16:34:04 -0300

    Two-Way-DataBind

--- Aula13/src/app/components/two-way-data-bind/two-way-data-bind.component.ts
+++ Aula13/src/app/components/two-way-data-bind/two-way-data-bind.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-two-way-data-bind',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './two-way-data-bind.component.html',
   styleUrl: './two-way-data-bind.component.css'
 })
