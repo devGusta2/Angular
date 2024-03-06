8aa495bd070ff50cbf4db88927b695492cc2bc5d
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-06 17:39:42 -0300

    Services

--- Aula14/src/app/app.component.ts
+++ Aula14/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Animal } from './Animal';
+import { ListService } from './servicos/list.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,5 +15,8 @@ export class AppComponent {
     {name:'Tom',age:'5',type:'Cat'},
     {name:'Fubá',age:'5',type:'dog'}
   ]
+
+
+  constructor(private listService: ListService){}
 
 }
