0f1abc2e5f3f234af7c569c5be68e45f7e6f6fd9
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-15 13:49:23 -0300

    Router

--- Aula15/src/app/app.routes.ts
+++ Aula15/src/app/app.routes.ts
@@ -1,6 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { RouterComponent } from './components/router/router.component';
+import { ListComponent } from './components/list/list.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    {path:'', component: RouterComponent}
+    {path:'', component: RouterComponent},
+    {path:'',component: ListComponent}, 
 ];
+
+@NgModule({
+    declarations:[],
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule],
+})
+
+export class AppRoutesModule{}
