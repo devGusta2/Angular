3df8bcd7c477c2a3997263c1bf770ca52e99c7e6
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-15 13:53:47 -0300

    Router

--- Aula15/src/app/app.component.ts
+++ Aula15/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AppRoutesModule } from './app.routes';
 @Component({
   selector: 'app-root',
   standalone: true,
