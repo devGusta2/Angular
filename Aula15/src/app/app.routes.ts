10fd2063ff62a276a4c3afc2c991aea6ed0ad06a
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-15 13:35:31 -0300

    Router

--- Aula15/src/app/app.routes.ts
+++ Aula15/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { RouterComponent } from './components/router/router.component';
 
 export const routes: Routes = [
-    {path:''}
+    {path:'', component: RouterComponent}
 ];
